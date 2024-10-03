d267f35b1d9a5435bed80b03663d2c8c906aa814
Jennifer Mejia Meliton <jenniux55@gmail.com>
2024-10-02 23:17:57 -0500

    practice

---
+++ UtilScanner/cat.java
@@ -0,0 +1,30 @@
+public class cat {
+
+    private String name;
+    private int age;
+
+    //constructor
+
+    public cat(){
+        this.name = "unknown";
+        this.age = 0;
+    }
+
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+    
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge() {
+        this.age = age;
+    }
+}
---
+++ UtilScanner/misce.java
@@ -0,0 +1,18 @@
+public class misce {
+
+    public static void main(String[] args) {
+        
+        String str1 = "Java is fun";
+        String str2 = "JAVA PROGRAMMING";
+        int result;
+
+        result = str1.indexOf("f");
+        System.out.println(result);
+
+        System.out.println(str2.charAt(8));
+        System.out.println(str2.toLowerCase());
+        
+        
+    }
+    
+}
